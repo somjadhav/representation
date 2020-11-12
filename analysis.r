e7370e2a84af78b5ceec4063126a2649b082f955
Som Jadhav <somjadhav787@gmail.com>
2020-11-12 17:22:20 -0600

    finished LIP

--- analysis.r
+++ analysis.r
@@ -1,6 +1,8 @@
 # set working directory and 
 setwd("C:/users/somja/Documents/Projects/representation")
 library(lpSolve)
+library(usmap)
+library(totalcensus)
 
 pop.est = read.csv("nst-est2019-alldata.csv")
 pop.est = pop.est[,1:27]
@@ -18,11 +20,12 @@ state.data$POPESTIMATE2020 <- state.data$POPESTIMATE2019*(1+state.data$cagr)
 state.data$CENSUSEST2020 <- state.data$POPESTIMATE2019*(1+state.data$cagr*9/12)
 total.pop <- sum(state.data$CENSUSEST2020)
 state.data$CENSUSEST2020.PCT <- state.data$CENSUSEST2020/total.pop
+rownames(state.data) <- NULL
 
 # columns 1-51 are for y sub i
 # columns 52-102 are for x sub i
 A = matrix(0,154,102)
-b = rep(0,102)
+b = rep(0,154)
 sense = rep('>',154)
 for (val in 1:51){
   index = val*2-1
@@ -32,8 +35,8 @@ for (val in 1:51){
   A[index,val+51] <- -1
   A[index+1,val+51] <- 1
   
-  b[index] <- -state.data$CENSUSEST2020.PCT*538
-  b[index+1] <- state.data$CENSUSEST2020.PCT*538
+  b[index] <- -state.data$CENSUSEST2020.PCT[val]*538
+  b[index+1] <- state.data$CENSUSEST2020.PCT[val]*538
   
   
 }
@@ -42,7 +45,7 @@ A[103:153,52:102] <- diag(51)
 b[103:153] <- 3
 
 #sum of all x sub i must equal 538
-A[154,] <- 1
+A[154,52:102] <- 1
 b[154] <- 538
 sense[154] <- "="
 
@@ -51,7 +54,11 @@ obj = rep(0,102)
 obj[1:51] <- 1
 
 # solve
-sln = lp("min",obj,A,sense,b,all.int = TRUE)
+sln = lp("min",obj,A,sense,b,int.vec = seq(52,102))
+sln$solution
 
-
-
+state.fips <- fips(state.data$NAME)
+state.names <- convert_fips_to_names(state.fips,states = NULL, geo_header = "STATE", in_states = "US")
+results = cbind(state.names,current.votes[6:56],sln$solution[52:102])
+results = data.frame(results)
+colnames(results) <- c("State","Current EC Votes", "Predicted EC Votes")
